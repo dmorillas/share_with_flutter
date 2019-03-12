eb78d7b3edb97888089cdf346b0f06f0fd2e2d27
Morillas <dani.morillas@king.com>
2019-03-12 13:09:46 +0100

    implemented the share on Facebook with callback for Android

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:share_with_flutter/share_with_flutter.dart';
 
@@ -12,32 +13,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await ShareWithFlutter.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -47,10 +25,72 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Container(
+          alignment: AlignmentDirectional.center,
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: onShareLinkOnFacebook,
+                child: Text(
+                  "Share Link on Facebook",
+                  style: TextStyle(
+                      fontSize: 16.0,
+                  ),
+                ),
+              ),
+              RaisedButton(
+                onPressed: onSharePictureOnFacebook,
+                child: Text(
+                  "Share Picture on Facebook",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              RaisedButton(
+                onPressed: onShareLinkOnFacebook,
+                child: Text(
+                  "Share on Twitter",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void onShareLinkOnFacebook() async {
+    await ShareWithFlutter.shareOnFacebook(
+        (String status, String message) async {
+          print("Result when sharing link on Facebook: $status");
+        },
+        link: "flutter.io");
+  }
+
+  void onSharePictureOnFacebook() async {
+    String imagePath = await getTempImagePath();
+
+    await ShareWithFlutter.shareOnFacebook(
+        (String status, String message) async {
+            print("Result when sharing image on Facebook: $status");
+        },
+        imagePath: imagePath);
+  }
+
+  Future<String> getTempImagePath() async {
+    ByteData byteData = await DefaultAssetBundle.of(context).load("assets/flutter.png");
+
+    final Uint8List list = byteData.buffer.asUint8List();
+    final tempDir = Directory.systemTemp;
+    final imagePath = '${tempDir.path}/temp.png';
+    final file = await new File(imagePath).create();
+    await file.writeAsBytes(list);
+
+    return imagePath;
   }
 }
--- lib/share_with_flutter.dart
+++ lib/share_with_flutter.dart
@@ -6,20 +6,42 @@ class ShareWithFlutter {
   static const MethodChannel _channel =
       const MethodChannel('share_with_flutter');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
-  Future<String> shareOnTwitter({String message = ''}) async {
+  static Future<String> shareOnTwitter({String message = '', String link = '', String imagePath = ''}) async {
     return await _channel.invokeMethod('shareOnTwitter', <String, dynamic>{
       'message': message,
     });
   }
 
-  Future<String> shareOnFacebook({String message = ''}) async {
-    return await _channel.invokeMethod('shareOnFacebook', <String, dynamic>{
-      'message': message,
+  static Future<void> shareOnFacebook(Function(String status, String message) onResult, {String link = '', String imagePath = ''}) async {
+    if(link.isEmpty && imagePath.isEmpty) {
+      print("shareOnFacebook: link and imagePath can not be both empty");
+      return "ERROR";
+    }
+
+    dynamic result =  await _channel.invokeMethod('shareOnFacebook', <String, dynamic> {
+      'link': link,
+      'imagePath': imagePath,
     });
+
+    processResult(result, onResult);
+  }
+
+  static void processResult(dynamic result, Function onResult) {
+    String status = result["status"];
+    String message = result["errorMessage"];
+    switch(status) {
+      case "SUCCESS":
+        print("Post shared successfully");
+        break;
+      case "CANCEL":
+        print("Shared cancelled");
+        break;
+      case "ERROR":
+        message = result["errorMessage"];
+        print("Some error ocurred: $message");
+        break;
+    }
+
+    onResult(status, message);
   }
 }
